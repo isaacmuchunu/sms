40ddbd4600a086baca60d3e16433991de7223786
isaacmuchunu <14983883+isaacmuchunu@users.noreply.github.com>
2025-05-21 13:21:17 -0700

    feat: server (47/235)

---
+++ server/utils/escapeHtml.js
@@ -0,0 +1,17 @@
+/**
+ * Escape special HTML characters in a string.
+ * @param {string} str
+ * @returns {string}
+ */
+const escapeHtml = (str) => {
+  if (str == null) return '';
+  return String(str)
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;')
+    .replace(/\//g, '&#x2F;');
+};
+
+module.exports = { escapeHtml };
