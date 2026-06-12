978d32e0fdb5439773ede65a1c4324cd387a7943
z <14983883+isaacmuchunu@users.noreply.github.com>
2026-06-12 03:25:11 -0700

    chore: import project file 026
    Add sms/client/src/main.jsx

---
+++ sms/client/src/main.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import App from './App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 4000,
+          style: {
+            fontFamily: 'Inter, system-ui, sans-serif',
+            fontSize: '0.875rem',
+          },
+          success: {
+            iconTheme: {
+              primary: '#10b981',
+              secondary: '#fff',
+            },
+          },
+          error: {
+            iconTheme: {
+              primary: '#ef4444',
+              secondary: '#fff',
+            },
+          },
+        }}
+      />
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
